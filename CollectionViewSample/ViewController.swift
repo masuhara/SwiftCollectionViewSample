df3dd536054917cc03fddfe0742a45fce1dca611
masuhara <info@masuhara.net>
2019-06-08 16:00:58 +0900

    Initial Commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by Masuhara on 2019/06/08.
+//  Copyright © 2019 Ylab Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
